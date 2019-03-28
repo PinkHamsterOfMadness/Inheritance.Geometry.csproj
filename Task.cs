fc39de663056fc24ef6820663035ccf8305b80d0
PinkHamsterOfMadness <pink.hamster.of.madness@gmail.com>
2019-03-28 23:24:07 +0300

    реализация паттерна посетитель

---
+++ Task.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance.Geometry
+{
+	public abstract class Body
+	{
+        public abstract Double GetVolume();
+        public abstract void Accept(IVisitor visitor);
+	}
+
+	public class Ball : Body
+	{
+		public double Radius { get; set; }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+        public override double GetVolume()
+        {
+            return 4.0 * Math.PI * Math.Pow(this.Radius, 3) / 3;
+        }
+    }
+
+	public class Cube : Body
+	{
+		public double Size { get; set; }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+        public override double GetVolume()
+        {
+            return Math.Pow(this.Size, 3);
+        }
+    }
+
+	public class Cyllinder : Body
+	{
+		public double Height { get; set; }
+		public double Radius { get; set; }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+        public override double GetVolume()
+        {
+            return Math.PI * Math.Pow(this.Radius, 2) * this.Height;
+        }
+    }
+
+    public interface IVisitor
+    {
+        void Visit(Cube cube);
+        void Visit(Ball ball);
+        void Visit(Cyllinder cyllinder);
+    }
+
+	// Заготовка класса для задачи на Visitor
+	public class SurfaceAreaVisitor : IVisitor
+	{
+		public double SurfaceArea { get; private set; }
+
+        public void Visit(Cube cube)
+        {
+            this.SurfaceArea = 6 * Math.Pow(cube.Size, 2);
+        }
+
+        public void Visit(Ball ball)
+        {
+            this.SurfaceArea = 4 * Math.PI * Math.Pow(ball.Radius, 2);
+        }
+
+        public void Visit(Cyllinder cyllinder)
+        {
+            this.SurfaceArea = 2 * Math.PI * cyllinder.Radius * (cyllinder.Radius + cyllinder.Height);
+        }
+    }
+	public class DimensionsVisitor : IVisitor
+	{
+		public Dimensions Dimensions { get; private set; }
+
+        public void Visit(Cube cube)
+        {
+            this.Dimensions = new Dimensions(cube.Size, cube.Size);
+        }
+
+        public void Visit(Ball ball)
+        {
+            this.Dimensions = new Dimensions(2 * ball.Radius, 2 * ball.Radius);
+        }
+
+        public void Visit(Cyllinder cyllinder)
+        {
+            this.Dimensions = new Dimensions(cyllinder.Radius * 2, cyllinder.Height);
+        }
+    }
+}
